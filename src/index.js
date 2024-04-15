7c487259c6072ebcf316bc65e8d84f28659bdc57
Veronica <veronica.stevania@gmail.com>
2024-04-16 00:46:09 +0200

    Add react to print

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React, {useRef} from 'react'
+// import ReactDOM from 'react-dom/client'
+import {render} from 'react-dom'
+import {useReactToPrint} from 'react-to-print'
+
 import './index.css'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+class ComponentToPrint extends React.Component {
+  render () {
+    return (
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    )
+  }
+}
+
+const Exemple = () => {
+  const componentRef = useRef()
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  })
+
+  return (
+    <div>
+      <ComponentToPrint ref={componentRef} />
+      <button onClick={handlePrint}>Print this out!</button>
+    </div>
+  )
+}
+
+render(<Exemple/>, document.querySelector("#root"))

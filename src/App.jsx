25e4c37ce92c941d478fbf15df049e2ad29fca65
Veronica <veronica.stevania@gmail.com>
2024-04-18 00:16:45 +0200

    Add description Array.from into App.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import "./App.css"
 
 function App() {
 
-const start = 1001
-const stop = 1100
-const step = 1
+  // Array.from() static method creates a new, shallow-copied Array instance from an iterable or array-like object.
+  // Sequence generator function (commonly referred to as "range", e.g. Clojure, PHP, etc.)
 
-return Array.from({ length: (stop- start) / step + 1 }, (_, i) => (
-    <div className="appPage">
-        <Ticket ticketNumber={start + i * step}/>             
-    </div>)
-  ) 
+  const start = 3401
+  const stop = 3500
+  const step = 1
+
+  return Array.from({ length: (stop- start) / step + 1 }, (_, i) => (
+      <div className="appPage">          
+        {/**Generate numbers range */}
+        <Ticket ticketNumber={start + i * step}/>                                  
+      </div>)
+    ) 
 }
-
 
 export default App
